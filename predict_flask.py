95e58825d46b6e52c5716aac706ff6ca572e5de1
fireae <fireae007@gmail.com>
2018-11-19 18:08:57 +0800

    predict flask

---
+++ predict_flask.py
@@ -0,0 +1,269 @@
+# import argparse
+
+import numpy as np
+from PIL import Image, ImageDraw
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input
+
+import cfg
+from label import point_inside_of_quad
+from east_net import East
+from preprocess import resize_image
+from nms import nms
+import tensorflow as tf
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse
+
+
+def init_model():
+    export_dir = 'prod_models/1/'
+    # with tf.Session(graph=tf.Graph()) as sess:
+    sess = tf.Session(graph=tf.Graph())
+    tf.saved_model.loader.load(sess, [tf.saved_model.tag_constants.SERVING], export_dir)
+    input = sess.graph.get_tensor_by_name('input_img:0')
+    output = sess.graph.get_tensor_by_name('east_detect/concat:0')
+    # predict_tf(sess, img_path, threshold)
+    return sess
+
+def sigmoid(x):
+    """`y = 1 / (1 + exp(-x))`"""
+    return 1 / (1 + np.exp(-x))
+
+
+def cut_text_line(geo, scale_ratio_w, scale_ratio_h, im_array, img_path, s):
+    geo /= [scale_ratio_w, scale_ratio_h]
+    p_min = np.amin(geo, axis=0)
+    p_max = np.amax(geo, axis=0)
+    min_xy = p_min.astype(int)
+    max_xy = p_max.astype(int) + 2
+    sub_im_arr = im_array[min_xy[1]:max_xy[1], min_xy[0]:max_xy[0], :].copy()
+    for m in range(min_xy[1], max_xy[1]):
+        for n in range(min_xy[0], max_xy[0]):
+            if not point_inside_of_quad(n, m, geo, p_min, p_max):
+                sub_im_arr[m - min_xy[1], n - min_xy[0], :] = 255
+    sub_im = image.array_to_img(sub_im_arr, scale=False)
+    sub_im.save(img_path + '_subim%d.jpg' % s)
+
+
+def predict(east_detect, img_path, pixel_threshold, quiet=False):
+    img = image.load_img(img_path)
+    d_wight, d_height = resize_image(img, cfg.max_predict_img_size)
+    img = img.resize((d_wight, d_height), Image.NEAREST).convert('RGB')
+    img = image.img_to_array(img)
+    img = preprocess_input(img, mode='tf')
+    x = np.expand_dims(img, axis=0)
+    y = east_detect.predict(x)
+
+    y = np.squeeze(y, axis=0)
+    y[:, :, :3] = sigmoid(y[:, :, :3])
+    cond = np.greater_equal(y[:, :, 0], pixel_threshold)
+    activation_pixels = np.where(cond)
+    quad_scores, quad_after_nms = nms(y, activation_pixels)
+    with Image.open(img_path) as im:
+        im_array = image.img_to_array(im.convert('RGB'))
+        d_wight, d_height = resize_image(im, cfg.max_predict_img_size)
+        scale_ratio_w = 1.0*d_wight / im.width
+        scale_ratio_h = 1.0*d_height / im.height
+        im = im.resize((d_wight, d_height), Image.NEAREST).convert('RGB')
+        quad_im = im.copy()
+        draw = ImageDraw.Draw(im)
+        for i, j in zip(activation_pixels[0], activation_pixels[1]):
+            px = (j + 0.5) * cfg.pixel_size
+            py = (i + 0.5) * cfg.pixel_size
+            line_width, line_color = 1, 'red'
+            if y[i, j, 1] >= cfg.side_vertex_pixel_threshold:
+                if y[i, j, 2] < cfg.trunc_threshold:
+                    line_width, line_color = 2, 'yellow'
+                elif y[i, j, 2] >= 1 - cfg.trunc_threshold:
+                    line_width, line_color = 2, 'green'
+            draw.line([(px - 0.5 * cfg.pixel_size, py - 0.5 * cfg.pixel_size),
+                       (px + 0.5 * cfg.pixel_size, py - 0.5 * cfg.pixel_size),
+                       (px + 0.5 * cfg.pixel_size, py + 0.5 * cfg.pixel_size),
+                       (px - 0.5 * cfg.pixel_size, py + 0.5 * cfg.pixel_size),
+                       (px - 0.5 * cfg.pixel_size, py - 0.5 * cfg.pixel_size)],
+                      width=line_width, fill=line_color)
+        im.save(img_path + '_act.jpg')
+        quad_draw = ImageDraw.Draw(quad_im)
+        txt_items = []
+        for score, geo, s in zip(quad_scores, quad_after_nms,
+                                 range(len(quad_scores))):
+            if np.amin(score) > 0:
+                quad_draw.line([tuple(geo[0]),
+                                tuple(geo[1]),
+                                tuple(geo[2]),
+                                tuple(geo[3]),
+                                tuple(geo[0])], width=2, fill='red')
+                if cfg.predict_cut_text_line:
+                    cut_text_line(geo, scale_ratio_w, scale_ratio_h, im_array,
+                                  img_path, s)
+                rescaled_geo = geo / [scale_ratio_w, scale_ratio_h]
+                rescaled_geo_list = np.reshape(rescaled_geo, (8,)).tolist()
+                txt_item = ','.join(map(str, rescaled_geo_list))
+                txt_items.append(txt_item + '\n')
+            elif not quiet:
+                print('quad invalid with vertex num less then 4.')
+        quad_im.save(img_path + '_predict.jpg')
+        if cfg.predict_write2txt and len(txt_items) > 0:
+            with open(img_path[:-4] + '.txt', 'w') as f_txt:
+                f_txt.writelines(txt_items)
+
+
+def predict_tf(sess, img_path, pixel_threshold, quiet=False):
+    img = image.load_img(img_path)
+    d_wight, d_height = resize_image(img, cfg.max_predict_img_size)
+    img = img.resize((d_wight, d_height), Image.NEAREST).convert('RGB')
+    img = image.img_to_array(img)
+    img = preprocess_input(img, mode='tf')
+    x = np.expand_dims(img, axis=0)
+    # y = east_detect.predict(x)
+    input = sess.graph.get_tensor_by_name('input_img:0')
+    output = sess.graph.get_tensor_by_name('east_detect/concat:0')
+    y = sess.run([output], feed_dict={'input_img:0':x})
+    print(y)
+    y = np.squeeze(y, axis=0)
+    y[:, :, :3] = sigmoid(y[:, :, :3])
+    cond = np.greater_equal(y[:, :, 0], pixel_threshold)
+    activation_pixels = np.where(cond)
+    quad_scores, quad_after_nms = nms(y, activation_pixels)
+    with Image.open(img_path) as im:
+        im_array = image.img_to_array(im.convert('RGB'))
+        d_wight, d_height = resize_image(im, cfg.max_predict_img_size)
+        scale_ratio_w = 1.0*d_wight / im.width
+        scale_ratio_h = 1.0*d_height / im.height
+        im = im.resize((d_wight, d_height), Image.NEAREST).convert('RGB')
+        quad_im = im.copy()
+        draw = ImageDraw.Draw(im)
+        for i, j in zip(activation_pixels[0], activation_pixels[1]):
+            px = (j + 0.5) * cfg.pixel_size
+            py = (i + 0.5) * cfg.pixel_size
+            line_width, line_color = 1, 'red'
+            if y[i, j, 1] >= cfg.side_vertex_pixel_threshold:
+                if y[i, j, 2] < cfg.trunc_threshold:
+                    line_width, line_color = 2, 'yellow'
+                elif y[i, j, 2] >= 1 - cfg.trunc_threshold:
+                    line_width, line_color = 2, 'green'
+            draw.line([(px - 0.5 * cfg.pixel_size, py - 0.5 * cfg.pixel_size),
+                       (px + 0.5 * cfg.pixel_size, py - 0.5 * cfg.pixel_size),
+                       (px + 0.5 * cfg.pixel_size, py + 0.5 * cfg.pixel_size),
+                       (px - 0.5 * cfg.pixel_size, py + 0.5 * cfg.pixel_size),
+                       (px - 0.5 * cfg.pixel_size, py - 0.5 * cfg.pixel_size)],
+                      width=line_width, fill=line_color)
+        im.save(img_path + '_act.jpg')
+        quad_draw = ImageDraw.Draw(quad_im)
+        txt_items = []
+        for score, geo, s in zip(quad_scores, quad_after_nms,
+                                 range(len(quad_scores))):
+            if np.amin(score) > 0:
+                quad_draw.line([tuple(geo[0]),
+                                tuple(geo[1]),
+                                tuple(geo[2]),
+                                tuple(geo[3]),
+                                tuple(geo[0])], width=2, fill='red')
+                if cfg.predict_cut_text_line:
+                    cut_text_line(geo, scale_ratio_w, scale_ratio_h, im_array,
+                                  img_path, s)
+                rescaled_geo = geo / [scale_ratio_w, scale_ratio_h]
+                rescaled_geo_list = np.reshape(rescaled_geo, (8,)).tolist()
+                txt_item = ','.join(map(str, rescaled_geo_list))
+                txt_items.append(txt_item + '\n')
+            elif not quiet:
+                print('quad invalid with vertex num less then 4.')
+        quad_im.save(img_path + '_predict.jpg')
+        if cfg.predict_write2txt and len(txt_items) > 0:
+            with open(img_path[:-4] + '.txt', 'w') as f_txt:
+                f_txt.writelines(txt_items)
+
+def predict_txt(east_detect, img_path, txt_path, pixel_threshold, quiet=False):
+    img = image.load_img(img_path)
+    d_wight, d_height = resize_image(img, cfg.max_predict_img_size)
+    scale_ratio_w = d_wight / img.width
+    scale_ratio_h = d_height / img.height
+    img = img.resize((d_wight, d_height), Image.NEAREST).convert('RGB')
+    img = image.img_to_array(img)
+    img = preprocess_input(img, mode='tf')
+    x = np.expand_dims(img, axis=0)
+    y = east_detect.predict(x)
+
+    y = np.squeeze(y, axis=0)
+    y[:, :, :3] = sigmoid(y[:, :, :3])
+    cond = np.greater_equal(y[:, :, 0], pixel_threshold)
+    activation_pixels = np.where(cond)
+    quad_scores, quad_after_nms = nms(y, activation_pixels)
+
+    txt_items = []
+    for score, geo in zip(quad_scores, quad_after_nms):
+        if np.amin(score) > 0:
+            rescaled_geo = geo / [scale_ratio_w, scale_ratio_h]
+            rescaled_geo_list = np.reshape(rescaled_geo, (8,)).tolist()
+            txt_item = ','.join(map(str, rescaled_geo_list))
+            txt_items.append(txt_item + '\n')
+        elif not quiet:
+            print('quad invalid with vertex num less then 4.')
+    if cfg.predict_write2txt and len(txt_items) > 0:
+        with open(txt_path, 'w') as f_txt:
+            f_txt.writelines(txt_items)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', '-p',
+                        default='demo/012.png',
+                        help='image path')
+    parser.add_argument('--threshold', '-t',
+                        default=cfg.pixel_threshold,
+                        help='pixel activation threshold')
+    return parser.parse_args()
+
+import os
+def save_model_for_production(model, version, path='prod_models'):
+    tf.keras.backend.set_learning_phase(1)
+    if not os.path.exists(path):
+        os.mkdir(path)
+    export_path = os.path.join(
+        tf.compat.as_bytes(path),
+        tf.compat.as_bytes(version))
+    builder = tf.saved_model.builder.SavedModelBuilder(export_path)
+
+    model_input = tf.saved_model.utils.build_tensor_info(model.input)
+    model_output = tf.saved_model.utils.build_tensor_info(model.output)
+
+    prediction_signature = (
+        tf.saved_model.signature_def_utils.build_signature_def(
+            inputs={'inputs': model_input},
+            outputs={'output': model_output},
+            method_name=tf.saved_model.signature_constants.PREDICT_METHOD_NAME))
+
+    with tf.keras.backend.get_session() as sess:
+        builder.add_meta_graph_and_variables(
+            sess=sess, tags=[tf.saved_model.tag_constants.SERVING],
+            signature_def_map={
+                'predict':
+                    prediction_signature,
+            })
+
+        builder.save()
+
+app = Flask(__name__)
+api = Api(app)
+
+req = reqparse.RequestParser()
+req.add_argument('image', type=str,required=True, location='Form')
+
+sess = init_model()
+
+
+class ImageAPI(Resource):
+    def get(self):
+        return 404
+
+    def post(self):
+        # args = reqparse.parse_
+        # image = args['image']
+        img_path = 'demo/012.png'
+        threshold = float(0.9)
+        print(img_path, threshold)
+
+        predict_tf(sess, img_path, threshold)
+api.add_resource(ImageAPI, '/image', endpoint='idcard')
+if __name__ == '__main__':
+    app.run()
